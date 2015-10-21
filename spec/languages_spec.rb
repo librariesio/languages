0453524580f481324df90646bf1756d978e874df
Alvaro F. Lara <alvaro.lara@alliants.com>
2015-10-21 23:26:31 +0100

    Allow finding languages by file extension

---
+++ spec/languages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'byebug'
+require 'languages'
+
+describe Languages::Language do
+  describe ".by_extension" do
+    it "returns an array of candidate languages by extension" do
+      ruby_language = described_class.find_by_name("Ruby")
+
+      expect(described_class.by_extension('.rb')).to eq([ruby_language])
+    end
+  end
+end
